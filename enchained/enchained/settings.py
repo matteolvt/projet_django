6777fc9e6e9c6671814160901ba02e1a0c504ba7
matteolvt <146728526+matteolvt@users.noreply.github.com>
2025-01-09 13:11:55 +0100

    modif settings

--- enchained/enchained/settings.py
+++ enchained/enchained/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'login', 
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+path('auth/', include('login.urls')),
