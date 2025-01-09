ba7b3d51af6ed0f47a2cc4401f25d29e8f55ed16
matteolvt <146728526+matteolvt@users.noreply.github.com>
2025-01-09 13:15:39 +0100

    modif urls

--- enchained/enchained/settings.py
+++ enchained/enchained/settings.py
@@ -133,4 +133,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-path('auth/', include('login.urls')),
--- enchained/enchained/urls.py
+++ enchained/enchained/urls.py
@@ -14,16 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('home.urls'))
+    path('products/', include('home.urls')),
+    path('auth/', include('login.urls')),
 ]
 
 if settings.DEBUG:
